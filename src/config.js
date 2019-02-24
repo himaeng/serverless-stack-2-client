15882bcfed312a42bddd298757c93a97b202fbf0
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2019-02-24 19:04:01 +0700

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "ap-southeast-1",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-17o3qkfjj70b0"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "ap-southeast-1",
+    URL: "https://api.fitlogs.fit/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_c44o0XEbI",
+    APP_CLIENT_ID: "5228tmjdo4o5e0rkgaq8i0nk69",
+    IDENTITY_POOL_ID: "ap-southeast-1:6296762e-4d9f-441f-9f26-de508cfde6aa"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-8xju2tgvichz"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://api.fitlogs.fit/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_yVVf1J0zR",
+    APP_CLIENT_ID: "7u7ja96josunv3oikcflcuhk1h",
+    IDENTITY_POOL_ID: "ap-southeast-1:98d91eae-cb18-456b-92fd-c899df03af48"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
